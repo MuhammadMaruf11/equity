c0b97d54ec256792ea65328a43cd11cea86c4be8
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2024-08-18 03:32:39 +0600

    about and contact page

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,39 +10,22 @@
     // Show the content
     document.getElementById('content').style.display = 'block';
 
-    // Load the video asynchronously
-    function loadVideo(videoId) {
-        const video = document.getElementById(videoId);
-        const videoSource = video.querySelector('source');
-        videoSource.src = videoSource.getAttribute('data-src');
-        video.load();
-    }
 
-    // Load each video asynchronously
-    loadVideo('video1');
-    loadVideo('video2');
-    loadVideo('video3');
 
-    const options = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1
-    };
+    /*------------------------------------
+              sideBarMenu - All Pages
+            --------------------------------------*/
 
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const videoId = entry.target.id;
-                loadVideo(videoId);
-                observer.unobserve(entry.target);
-            }
-        });
-    }, options);
-
-    document.querySelectorAll('.background-video').forEach(video => {
-        observer.observe(video);
+    $("#menuBtn").on("click", function (event) {
+        event.preventDefault();
+        console.log("object");
+        $(".side-bar-menu").addClass("active");
     });
 
+    $("#crossBtn").on("click", function (event) {
+        event.preventDefault();
+        $(".side-bar-menu").removeClass("active");
+    });
 
 
     /*------------------------------------
@@ -65,165 +48,7 @@
 
     $('.venobox').venobox();
 
-    /*------------------------------------
-         Hero  Slider - index.html 
-       --------------------------------------*/
 
-    const heroSwiperContainer = document.querySelector('.hero-slider-active .swiper-container')
-
-    // Slider With Thumbs
-    if (jQuery(heroSwiperContainer).length > 0) {
-        let destinationSlider = new Swiper(heroSwiperContainer, {
-            // Optional parameters
-            slidesPerView: 1,
-            slidesPerColumn: 1,
-            loop: true,
-
-            autoplay: {
-                delay: 5000,
-            },
-
-            a11y: false
-
-        });
-    }
-
-
-    /*------------------------------------
-           Project  Slider - index.html 
-         --------------------------------------*/
-
-    const projectSwiperContainer = document.querySelector('.project-slider-active .swiper-container')
-
-    // Slider With Thumbs
-    if (jQuery(projectSwiperContainer).length > 0) {
-        let destinationSlider = new Swiper(projectSwiperContainer, {
-            // Optional parameters
-            slidesPerView: 4,
-            slidesPerColumn: 1,
-            loop: true,
-
-            autoplay: {
-                delay: 2000,
-            },
-
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                },
-                768: {
-                    slidesPerView: 2,
-                },
-                1024: {
-                    slidesPerView: 3,
-                },
-                1200: {
-                    slidesPerView: 4,
-                },
-            },
-
-            a11y: false
-
-        });
-    }
-
-    const projectBtnPrev = document.querySelector('.project-slider-button .btn-prev');
-    const projectBtnNext = document.querySelector('.project-slider-button .btn-next');
-
-    projectBtnPrev.addEventListener('click', () => {
-        projectSwiperContainer.swiper.slidePrev();
-    });
-
-    projectBtnNext.addEventListener('click', () => {
-        projectSwiperContainer.swiper.slideNext();
-    });
-
-
-
-    /*------------------------------------
-          feature slider - index.html 
-         --------------------------------------*/
-
-    const featureSwiperContainer = document.querySelector('.feature-slider-active .swiper-container')
-    // Slider With Thumbs
-    if (jQuery(featureSwiperContainer).length > 0) {
-        let destinationSlider = new Swiper(featureSwiperContainer, {
-            // Optional parameters
-            slidesPerView: 1,
-            slidesPerColumn: 1,
-            loop: true,
-
-            autoplay: {
-                delay: 20000000,
-            },
-
-            a11y: false
-
-        });
-    }
-
-
-    const featureBtnPrev = document.querySelector('.feature-slider-button .btn-prev');
-    const featureBtnNext = document.querySelector('.feature-slider-button .btn-next');
-
-    featureBtnPrev.addEventListener('click', () => {
-        featureSwiperContainer.swiper.slidePrev();
-    });
-
-    featureBtnNext.addEventListener('click', () => {
-        featureSwiperContainer.swiper.slideNext();
-    });
-
-
-    /*------------------------------------
-         gallery  Slider - index.html 
-       --------------------------------------*/
-
-
-    const gallerySwiperContainer = document.querySelector('.gallery-slider-active .swiper-container')
-
-    // Slider With Thumbs
-    if (jQuery(gallerySwiperContainer).length > 0) {
-        let destinationSlider = new Swiper(gallerySwiperContainer, {
-            // Optional parameters
-            slidesPerView: 3,
-            slidesPerColumn: 1,
-            loop: true,
-            centeredSlides: true,
-            spaceBetween: 54,
-            autoplay: {
-                delay: 5000,
-            },
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                },
-                768: {
-                    slidesPerView: 2,
-                },
-                1024: {
-                    slidesPerView: 3,
-                },
-                1200: {
-                    slidesPerView: 3,
-                },
-            },
-
-            a11y: false
-
-        });
-    }
-
-    const galleryBtnPrev = document.querySelector('.gallery-slider-button .btn-prev');
-    const galleryBtnNext = document.querySelector('.gallery-slider-button .btn-next');
-
-    galleryBtnPrev.addEventListener('click', () => {
-        gallerySwiperContainer.swiper.slidePrev();
-    });
-
-    galleryBtnNext.addEventListener('click', () => {
-        gallerySwiperContainer.swiper.slideNext();
-    });
 
 
     /*------------------------------------
@@ -233,7 +58,7 @@
 
     AOS.init({
         duration: 800,
-        once: true,
+        // once: true,
     });
 
     /*------------------------------------
