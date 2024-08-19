96cc466fe22bd6895810f4cb384c2c3a3deb5e15
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2024-08-20 04:46:53 +0600

    projects and details create

---
+++ assets/js/feature-slide-active.js
@@ -0,0 +1,34 @@
+/*------------------------------------
+      feature slider - index.html 
+     --------------------------------------*/
+
+const featureSwiperContainer = document.querySelector('.feature-slider-active .swiper-container')
+// Slider With Thumbs
+if (jQuery(featureSwiperContainer).length > 0) {
+    let destinationSlider = new Swiper(featureSwiperContainer, {
+        // Optional parameters
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        loop: true,
+
+        autoplay: {
+            delay: 20000000,
+        },
+
+        a11y: false
+
+    });
+}
+
+
+const featureBtnPrev = document.querySelector('.feature-slider-button .btn-prev');
+const featureBtnNext = document.querySelector('.feature-slider-button .btn-next');
+
+featureBtnPrev.addEventListener('click', () => {
+    featureSwiperContainer.swiper.slidePrev();
+});
+
+featureBtnNext.addEventListener('click', () => {
+    featureSwiperContainer.swiper.slideNext();
+});
+
--- assets/js/swiper-active.js
+++ assets/js/swiper-active.js
@@ -77,42 +77,6 @@ projectBtnNext.addEventListener('click', () => {
 });
 
 
-
-/*------------------------------------
-      feature slider - index.html 
-     --------------------------------------*/
-
-const featureSwiperContainer = document.querySelector('.feature-slider-active .swiper-container')
-// Slider With Thumbs
-if (jQuery(featureSwiperContainer).length > 0) {
-    let destinationSlider = new Swiper(featureSwiperContainer, {
-        // Optional parameters
-        slidesPerView: 1,
-        slidesPerColumn: 1,
-        loop: true,
-
-        autoplay: {
-            delay: 20000000,
-        },
-
-        a11y: false
-
-    });
-}
-
-
-const featureBtnPrev = document.querySelector('.feature-slider-button .btn-prev');
-const featureBtnNext = document.querySelector('.feature-slider-button .btn-next');
-
-featureBtnPrev.addEventListener('click', () => {
-    featureSwiperContainer.swiper.slidePrev();
-});
-
-featureBtnNext.addEventListener('click', () => {
-    featureSwiperContainer.swiper.slideNext();
-});
-
-
 /*------------------------------------
      gallery  Slider - index.html 
    --------------------------------------*/
